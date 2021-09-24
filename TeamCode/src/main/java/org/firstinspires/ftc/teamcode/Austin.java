afdbbdb0ceb2349f6727a4b7fba162a50a626553
austindeng <austindeng3@gmail.com>
2021-09-23 20:45:46 -0500

    Test push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Austin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Austin.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Austin {
+    // The Joys of Mac - a haiku by Austin Deng
     //Mac is so awful
     //I should buy a new laptop
     //This is super dumb
+
 }
