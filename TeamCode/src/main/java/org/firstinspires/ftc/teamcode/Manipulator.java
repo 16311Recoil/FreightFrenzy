2d03b5cbe593e874a8487efbbc870751a4e2872d
AdityaAngajala <aditya.angajala01@k12.leanderisd.org>
2021-10-08 17:36:37 -0500

    Manipulator Feedback + Assignments Kinda

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manipulator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manipulator.java
@@ -2,50 +2,93 @@ package org.firstinspires.ftc.teamcode;
 
 import androidx.annotation.NonNull;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Manipulator {
+    private LinearOpMode linear_OpMode;
+    private OpMode iterative_OpMode;
+
     DcMotor flyWheel;
 
-    Servo shoulder, turret;
+    DcMotor armRotator; // (previous shoulder) this is a motor not a servo
     Servo grabber;
     Servo magSwitch;
-    // TODO: Get actual arm measurements
-    double height = 10;
 
-    public Manipulator(OpMode opMode) {
-        flyWheel = opMode.hardwareMap.get(DcMotor.class,"duck_wheel");
-        shoulder = opMode.hardwareMap.get(Servo.class,  "servo180");
+    //define all of your private variables at the top
+
+
+    private final double GRAB = 0;
+    private final double UNGRAB = 0;
+    private final double MAG_ON = 0;
+    private final double MAG_OFF = 0;
+    private final double DUCK_POWER = 0;
+    private final int DUCK_TIME = 0;
+
+    public Manipulator(LinearOpMode opMode) { //Need a constructor for both LinearOpmode and Opmode
+        this.linear_OpMode = opMode;
+
+        flyWheel = opMode.hardwareMap.get(DcMotorEx.class,"duck_wheel");
+        armRotator = opMode.hardwareMap.get(DcMotorEx.class,  "armRotator");
         grabber = opMode.hardwareMap.get(Servo.class,   "grabbyGrabGrab");
         magSwitch = opMode.hardwareMap.get(Servo.class, "magSwitch");
-        turret = opMode.hardwareMap.get(Servo.class,    "turret");
+
+        flyWheel.setDirection(DcMotorSimple.Direction.FORWARD);
+        armRotator.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        flyWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armRotator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         opMode.telemetry.addLine("Manipulator Init Completed");
         opMode.telemetry.update();
     }
 
+    public Manipulator(OpMode opMode) {
+        this.iterative_OpMode = opMode;
+
+        flyWheel = opMode.hardwareMap.get(DcMotorEx.class,"duck_wheel");
+        armRotator = opMode.hardwareMap.get(DcMotorEx.class,  "armRotator");
+        grabber = opMode.hardwareMap.get(Servo.class,   "grabbyGrabGrab");
+        magSwitch = opMode.hardwareMap.get(Servo.class, "magSwitch");
+
+        flyWheel.setDirection(DcMotorSimple.Direction.FORWARD);
+        armRotator.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        flyWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        armRotator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        opMode.telemetry.addLine("Manipulator Init Completed");
+        opMode.telemetry.update();
+    }
+
+    //use camel case so basically first word uncapital every other word capital ex: moveTo instead of MoveTo
+
+
+    //Turret methods wouldn't exist in this class
+
+    //Detection isn't part of this class either, that fuctionality would be added in the robot class probably
+
     // --- Low-level Arm Control Functions -- //
 
-    public void SetArmAngle(double raise_angle, double turret_angle){
-        shoulder.setPosition(raise_angle);
-        turret.setPosition(turret_angle);
+    public void mechGrab(){
+        grabber.setPosition(GRAB);
     }
 
-    public void MechGrab(){
-        grabber.setPosition(180);
+    public void mechRelease(){
+        grabber.setPosition(UNGRAB);
     }
 
-    public void MechRelease(){
-        grabber.setPosition(0);
+    public void magGrab() {
+        magSwitch.setPosition(MAG_ON);
     }
 
-    public void MagGrab() {
-        magSwitch.setPosition(180);
-    }
-
-    public void MagRelease(){
+    public void magRelease(){
         magSwitch.setPosition(0);
     }
 
@@ -54,51 +97,61 @@ public class Manipulator {
     /**
      * Enable all holds
      */
-    public void Grab(){
-        MechGrab();
-        MagGrab();
+    public void grab(){
+        mechGrab();
+        magGrab();
     }
 
     /**
      * Release all holds
      */
-    public void Release(){
-        MechRelease();
-        MagRelease();
-    }
-
-    /**
-     * Moves arm to specific position in XYZ coordinates relative to the robot
-     * @param x distance in front of
-     * @param y distance to the side of
-     * @param z height of
-     */
-    public void MoveArmTo(double x, double y, double z){
-
-        double azimuth = Math.atan2(y, x);
-        double dist_to = Math.sqrt(x * x + y * y);
-        double elevation = Math.atan2(z - height, dist_to);
-        SetArmAngle(elevation, azimuth);
+    public void release(){
+        mechRelease();
+        magRelease();
     }
 
     // --- Duck Functions --- //
 
-    public boolean DetectDuck(){
-        return true; // TODO: Detect duck. Probably done in Austin's sensor class
-    }
-
-    public void GrabDucks(){
-        // Made non-blocking with threading
-        for (int i = 0; i < 8; i ++){
-            while (DetectDuck()) { // Just in case
-                GrabDuck();
-            }
-        }
-    }
-
-    public void GrabDuck(){
-        flyWheel.setPower(5); // TODO: Figure out duck power
-        // TODO: find a way to wait
+    public void grabDuck() throws InterruptedException{
+        flyWheel.setPower(DUCK_POWER); // TODO: Figure out duck power
+        Thread.sleep(DUCK_TIME);
+        // TODO: find a way to wait- Its Thread.sleep() and you have to have the method throw InterupptedEception Error
         flyWheel.setPower(0);
     }
+
+
+    //TODO for STAS + ANYA
+
+    public void toggleGrabber(){
+    }
+
+    public void toggleMagnet(){
+    }
+
+    public void setArmRotatorPower(){
+
+    }
+
+    public void goToLevel(double level){
+        //knowing that you are dealing with a motor for the armRotator,
+        // figure out how you can stop the motor at the correct height for the floor, bottom, middle and top levels (0, 1, 2, 3)
+        // (TIP: You need to stall out the motor once you get to the destired height)
+        // (TIP: Use motorName.getCurrentPosition() to get the encoder value, but don't worry about converting to inches yet)
+        // Might to make multiple variables or other methods to properly do
+    }
+
+    public void placeLevel(double level){
+        goToLevel(level);
+        release();
+    }
+
+    public void grabFloor(){
+        release();
+        goToLevel(0);
+        grab();
+    }
+
+
+
+
 }
