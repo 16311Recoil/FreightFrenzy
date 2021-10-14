613bf11776b37e4a942493559ac928c33087ec06
Hillo111 <stas.gannutin@gmail.com>
2021-10-14 11:18:08 -0500

    Set up turret class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Turret.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.apache.commons.math3.geometry.euclidean.twod.Line;
+
+public class Turret {
+    private LinearOpMode linear_OpMode;
+    private OpMode iterative_OpMode;
+
+    DcMotor turretMotor;
+
+    private double angle = 0;
+    private int targetAngle = 0;
+    private double lastRobotDirection = 0;
+
+    private final double ROTATION_SPEED = 0;
+
+    public Turret(LinearOpMode opMode){
+        linear_OpMode = opMode;
+
+        turretMotor = opMode.hardwareMap.get(DcMotor.class, "turret_motor");
+        turretMotor.setPower(0);
+        turretMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        turretMotor.setTargetPosition(0);
+        turretMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        opMode.telemetry.addLine("Turret init completed");
+        opMode.telemetry.update();
+    }
+
+    public Turret(OpMode opMode){
+        iterative_OpMode = opMode;
+
+        turretMotor = opMode.hardwareMap.get(DcMotor.class, "turret_motor");
+        turretMotor.setPower(0);
+        turretMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        turretMotor.setTargetPosition(0);
+        turretMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        opMode.telemetry.addLine("Turret init completed");
+        opMode.telemetry.update();
+    }
+
+    public void setDirection(int angle){
+        targetAngle = angle;
+    }
+
+    public void update(double robotDirection, double deltaTime){
+        turretMotor.setTargetPosition(nearestAngleToTurret((int)(targetAngle - robotDirection)));
+    }
+
+    public int nearestAngleToTurret(int angle){
+        int turretAngle = turretMotor.getCurrentPosition();
+        int normalizedAngle = normalizeAngle(angle), normalizedTurret = normalizeAngle(turretAngle);
+        int invert = 1;
+        if ((normalizedAngle < 90 && normalizedTurret > 270) || (normalizedTurret < 90 && normalizedAngle > 270))
+            invert = -1;
+
+        int change = Math.abs(normalizedAngle - normalizedTurret);
+        change = change > 180 ? 360 - change : change;
+
+        return turretAngle + change * (int)Math.signum(normalizedTurret - normalizedAngle) * invert;
+    }
+
+    public int normalizeAngle(int angle){
+        return ((angle % 360) + 360) % 360;
+    }
+}
