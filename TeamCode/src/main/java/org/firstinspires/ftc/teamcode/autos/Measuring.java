474c727f5fcc690ce567a162c041f1288fd79f6d
Hillo111 <stas.gannutin@gmail.com>
2021-11-30 08:03:29 -0600

    Make measuring work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Measuring.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/Measuring.java
@@ -1,26 +1,31 @@
 package org.firstinspires.ftc.teamcode.autos;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Drivetrain;
 
 @TeleOp(name="Measuring", group="utility")
-public class Measuring extends OpMode {
+public class Measuring extends LinearOpMode {
     Drivetrain drivetrain;
+    int c = 0;
 
     @Override
-    public void init(){
+    public void runOpMode(){
         drivetrain = new Drivetrain(this);
+        drivetrain.lowerOdom();
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+            int[] vals = drivetrain.getEncoders();
+            telemetry.addData("FF", vals[0]);
+            telemetry.addData("SF", vals[2]);
+            telemetry.addData("random int", c);
+            telemetry.update();
+            c++;
+        }
     }
 
-    @Override
-    public void loop(){
-        int[] vals = drivetrain.getEncoders();
-        telemetry.addData("F", vals[0]);
-        telemetry.addData("R", vals[1]);
-        telemetry.addData("L", vals[2]);
-        telemetry.addData("B", vals[3]);
-        telemetry.update();
-    }
 }
