1d7baad606dbee13711902af0c3bb193d49c141a
Hillo111 <stas.gannutin@gmail.com>
2021-12-03 11:49:11 -0600

    Partially do Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/BlueAutoPathed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/BlueAutoPathed.java
@@ -1,25 +1,93 @@
 package org.firstinspires.ftc.teamcode.autos;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Crab;
 import org.firstinspires.ftc.teamcode.Drivetrain;
+import org.firstinspires.ftc.teamcode.Manipulator;
 import org.firstinspires.ftc.teamcode.PID;
+import org.firstinspires.ftc.teamcode.VisionTest;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous(name="BlueAutoPathed", group="Auto")
 public class BlueAutoPathed extends LinearOpMode {
-    Drivetrain drivetrain;
+    Crab robot;
+    VisionTest.DeterminationPipeline pipeline;
+    FtcDashboard dashboard;
+    VisionTest.DeterminationPipeline.MarkerPosition pos;
 
     @Override
     public void runOpMode(){
-        drivetrain = new Drivetrain(this);
-        drivetrain.lowerOdom();
+        robot = new Crab(this);
+
+        robot.getDrivetrain().lowerOdom();
+        dashboard = FtcDashboard.getInstance();
+
+        pipeline = new VisionTest.DeterminationPipeline();
+        robot.getSensors().getWebcam().setPipeline(pipeline);
+        robot.getSensors().getWebcam().openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                robot.getSensors().getWebcam().startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+
+        while (!isStarted()) {
+            TelemetryPacket p = new TelemetryPacket();
+            dashboard.startCameraStream(robot.getSensors().getWebcam(), 30);
+
+            telemetry.addData("pos", pipeline.getAnalysis());
+            p.put("pos", pipeline.getAnalysis());
+            dashboard.sendTelemetryPacket(p);
+
+            pos = pipeline.getAnalysis();
+        }
+
 
         waitForStart();
 
-        // -46184, 7908
-        // TODO: Make the actual path
-        drivetrain.runToEncoderPositionsOdom(-46184, 7908);
+        robot.getTurret().setPosition(-90);
+
+
+        // robot.getManip().mechGrab();
+
+        int hub_pos;
+        if (pos == VisionTest.DeterminationPipeline.MarkerPosition.LEFT)
+            hub_pos = 1;
+        else if (pos == VisionTest.DeterminationPipeline.MarkerPosition.CENTER)
+            hub_pos = 2;
+        else
+            hub_pos = 3;
+
+        // TODO: Uncomment manip code after adjusting values in manip class (check manip TODOs)
+
+        // raise arm BEFORE we move forward
+        robot.getManip().placePresetLevel(hub_pos);
+        // manipulator.rotateClawUp();
+
+        // move towards the hub
+        robot.getDrivetrain().moveInches(18, 0.3, false, true);
+
+        // drop block
+        // manipulator.mechRelease();
+
+        // go back
+        robot.getDrivetrain().moveInches(-18, 0.3, false, true);
+
+        // TODO: Go to duck
+        // TODO: Get duck
+
+        // park in freight area
+        robot.getDrivetrain().moveInches(-50, 0.5, true, true);
     }
 }
